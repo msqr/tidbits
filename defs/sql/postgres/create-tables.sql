36e8e092141ba149c13bd3d8d6345f2dbe36a339
Matt Magoffin <github.com@msqr.us>
2009-06-23 09:30:55 +0000

    Update to HyperJAXB2 0.62; clean up database schema.

--- defs/sql/postgres/create-tables.sql
+++ defs/sql/postgres/create-tables.sql
@@ -1,8 +1,8 @@
-alter table tidbit drop constraint FKCBE8BE1E804D9E2A;
+alter table tidbit drop constraint tidbit_kind_fk;
 drop table tidbit;
 drop table tidbit_kind;
 drop sequence hibernate_sequence;
-create table tidbit (TidbitId int8 not null, Hjtype varchar(255) not null, Data text, Comment varchar(2048), Kind int8, CreatedBy varchar(64) not null, Name varchar(128) not null, ModifyDate timestamp with time zone, CreationDate timestamp with time zone not null, primary key (TidbitId));
-create table tidbit_kind (KindId int8 not null, Hjtype varchar(255) not null, Comment varchar(2048), CreatedBy varchar(64) not null, Name varchar(128) not null, ModifyDate timestamp with time zone, CreationDate timestamp with time zone not null, primary key (KindId));
-alter table tidbit add constraint FKCBE8BE1E804D9E2A foreign key (Kind) references tidbit_kind;
+create table tidbit (tidbitid int8 not null, data text, comment varchar(2048), kindid int8, created_by varchar(64) not null, name varchar(128) not null, modify_date timestamp with time zone, creation_date timestamp with time zone not null, primary key (tidbitid));
+create table tidbit_kind (kindid int8 not null, comment varchar(2048), created_by varchar(64) not null, name varchar(128) not null, modify_date timestamp with time zone, creation_date timestamp with time zone not null, primary key (kindid));
+alter table tidbit add constraint tidbit_kind_fk foreign key (kindid) references tidbit_kind;
 create sequence hibernate_sequence;
