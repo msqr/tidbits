cd540f7a61bd5fe83a4ef7e672179a365e8c2b9c
msqr <msqr@fa86e5af-965a-44d9-bc36-2d652c5e00f5>
2012-06-28 03:55:06 +0000

    Initial work on Import CSV.

---
+++ tidbits/trunk/src/magoffin/matt/tidbits/web/ImportFileForm.java
@@ -0,0 +1,69 @@
+/* ===================================================================
+ * ImportFileForm.java
+ * 
+ * Created Jun 28, 2012 1:52:18 PM
+ * 
+ * Copyright (c) 2012 Matt Magoffin.
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ===================================================================
+ * $Id$
+ * ===================================================================
+ */
+
+package magoffin.matt.tidbits.web;
+
+import java.util.List;
+import magoffin.matt.tidbits.domain.Tidbit;
+import magoffin.matt.util.TemporaryFile;
+
+/**
+ * Form for importing file data into Tidbits.
+ * 
+ * @author matt
+ * @version $Revision$ $Date$
+ */
+public class ImportFileForm {
+
+	private String page;
+	private TemporaryFile file;
+	private List<Tidbit> tidbits;
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public TemporaryFile getFile() {
+		return file;
+	}
+
+	public void setFile(TemporaryFile file) {
+		this.file = file;
+	}
+
+	public List<Tidbit> getTidbits() {
+		return tidbits;
+	}
+
+	public void setTidbits(List<Tidbit> tidbits) {
+		this.tidbits = tidbits;
+	}
+
+}
